ee97fddc158028c909665758be7f768f6b3d6a9b
Callum Sergeant <callum@ondaweb.co.uk>
2021-04-08 17:38:02 +0100

    reorder list for higher old papers

--- generatelist.py
+++ generatelist.py
@@ -1,9 +1,9 @@
-a = 2
-for i in range(1977, 2000):
-    a = a + 1
+a = 2000
+for i in range(1, 25):
+    a = a - 1
     print('''<tr>
-    <th scope="row">''', i, '''</th>
-    <td>''', i, ''' Past Paper</td>
-    <td><a href="H Chem Archive PP/oldoldHchem PP ''', i, '''.pdf">Past Paper</a>
+    <th scope="row">''', a, '''</th>
+    <td>''', a, ''' Past Paper</td>
+    <td><a href="H Chem Archive PP/oldoldHchem PP ''', a, '''.pdf">Past Paper</a>
     </td>
 </tr>''', sep='')
