8dc318de2228b71679b34082e0806159b4b430c9
Callum Sergeant <callum@ondaweb.co.uk>
2021-08-08 19:00:12 +0100

    Maths release version, change to jabstem

--- generatelist.py
+++ generatelist.py
@@ -1,9 +1,8 @@
-a = 2015
-for i in range(1, 16):
+a = 2013
+for i in range(1, 18):
     a = a - 1
     print('''<tr>
-        <th scope="row">''',a,'''</th>
-        <td><a href="int1/Int1SQAPP/Int1SQAPP''', a ,'''.pdf">''',a,''' Past Paper</a></td>
-        <td><a href="int1/Int1JABchemMsch/Int1JABchemMsch''',a,'''.pdf">JABchem Solutions</a></td>
-        <td><a href="int1/Int1SQAMsch/Int1SQAMsch''',a,'''.pdf">SQA Solutions</a></td>
+    <th scope="row">''',a,'''</th>
+    <td><a href="SGGeneralSQAPP/SGGeneralMathsSQApp''',a,'''.pdf">''',a,''' Past Paper</a></td>
+    <td><a href="SGGeneralSQAMSch/SGGeneralMathsSQAmsch''',a,'''.pdf">SQA Solutions</a></td>
 </tr>''', sep='')
--- removespaces.py
+++ removespaces.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-for f in glob.glob('**/*.pdf', recursive=True):
+for f in glob.glob('/*', recursive=True):
     print(f)
     newname = f.replace(' ', '')
     os.rename(f, newname)
